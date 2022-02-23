15a169cff8d759ccf0da332d3983eea1e2d282e5
yyl2020 <45748624+yyl2020@users.noreply.github.com>
2022-02-23 17:19:01 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
 
 <style>
 .app {
-  font-family: Muyao, Avenir, Helvetica, Arial, sans-serif;
+  font-family: LXGWMunHaeHangul-L, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
